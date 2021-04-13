6bd81dd683205deb39ea11895496462162d5f5a0
Miguel Angel Luna <miluna1994@gmail.com>
2021-04-14 00:57:49 +0200

    repository logic

--- src/main/java/com/miluna/wedding_guests/infrastructure/dynamodb/DynamoDbWeddingGuestRepository.java
+++ src/main/java/com/miluna/wedding_guests/infrastructure/dynamodb/DynamoDbWeddingGuestRepository.java
@@ -1,9 +1,12 @@
 package com.miluna.wedding_guests.infrastructure.dynamodb;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.GetItemResult;
-import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.miluna.wedding_guests.domain.*;
 import com.miluna.wedding_guests.infrastructure.WeddingGuestRepository;
 
@@ -14,41 +17,53 @@ import java.util.Optional;
 public class DynamoDbWeddingGuestRepository implements WeddingGuestRepository {
 
     private final AmazonDynamoDB amazonDynamoDB;
+    private final Table table;
 
     public DynamoDbWeddingGuestRepository(AmazonDynamoDB amazonDynamoDB) {
+        DynamoDB dynamoDB = new DynamoDB(amazonDynamoDB);
+        this.table = dynamoDB.getTable("wedding_guests");
         this.amazonDynamoDB = amazonDynamoDB;
     }
 
     @Override
     public void save(WeddingGuest weddingGuest) {
-        Map<String, AttributeValue> attributes = this.mapDomain(weddingGuest);
-        PutItemRequest request = new PutItemRequest("wedding_guests", attributes);
-        amazonDynamoDB.putItem(request);
+        Item item = this.mapDomain(weddingGuest);
+        table.putItem(item);
     }
 
     @Override
     public Optional<WeddingGuest> findByEmail(WeddingGuestEmail email) {
+        Optional<Item> result = Optional.empty();
+
+
         Map<String, AttributeValue> attributes = new HashMap<>();
-        attributes.put("email", new AttributeValue(email.getValue()));
-        GetItemResult result = amazonDynamoDB.getItem("wedding_guests", attributes);
-        return Optional.ofNullable(this.mapItem(result.getItem()));
+        attributes.put(":val", new AttributeValue().withS(email.getValue()));
+        ScanRequest request = new ScanRequest()
+                .withTableName("wedding_guests")
+                .withFilterExpression("email = :val")
+                .withExpressionAttributeValues(attributes)
+                .withLimit(1);
+        ScanResult scanResult = amazonDynamoDB.scan(request);
+
+        if (scanResult.getItems().isEmpty()) return Optional.empty();
+        Optional<Map<String, AttributeValue>> first = scanResult.getItems().stream().findFirst();
+        return first.map(this::mapItem);
     }
 
-    private WeddingGuest mapItem(Map<String, AttributeValue> item) {
+    private WeddingGuest mapItem(Map<String, AttributeValue> map) {
         return WeddingGuest.builder()
-                .weddingGuestId(new WeddingGuestId(item.get("weddingGuestId").getS()))
-                .name(new WeddingGuestName(item.get("name").getS()))
-                .email(new WeddingGuestEmail(item.get("email").getS()))
-                .telephone(new WeddingGuestTelephone(item.get("telephone").getS()))
+                .weddingGuestId(new WeddingGuestId(map.get("weddingGuestId").getS()))
+                .name(new WeddingGuestName(map.get("name").getS()))
+                .email(new WeddingGuestEmail(map.get("email").getS()))
+                .telephone(new WeddingGuestTelephone(map.get("telephone").getS()))
                 .build();
     }
 
-    private Map<String, AttributeValue> mapDomain(WeddingGuest source) {
-        Map<String, AttributeValue> attributes = new HashMap<>();
-        attributes.put("weddingGuestId", new AttributeValue(source.getWeddingGuestId().getValue()));
-        attributes.put("name", new AttributeValue(source.getName().getValue()));
-        attributes.put("email", new AttributeValue(source.getEmail().getValue()));
-        attributes.put("telephone", new AttributeValue(source.getTelephone().getValue()));
-        return attributes;
+    private Item mapDomain(WeddingGuest source) {
+        return new Item()
+                .withPrimaryKey("weddingGuestId", source.getWeddingGuestId().getValue())
+                .withString("name", source.getName().getValue())
+                .withString("email", source.getEmail().getValue())
+                .withString("telephone", source.getTelephone().getValue());
     }
 }
